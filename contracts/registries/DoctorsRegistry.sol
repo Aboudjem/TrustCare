51a57cb6eb51097055753d8682056ba84f7176a7
joachimlebrun <joachim@tokeny.com>
2020-09-23 16:54:45 +0200

    ♻(contracts) modify contracts

--- contracts/registries/DoctorsRegistry.sol
+++ contracts/registries/DoctorsRegistry.sol
@@ -1,14 +1,16 @@
 pragma solidity 0.6.2;
 
 import "../app/TrustCare.sol";
-import "../app/TrustCare.sol";
+import "../roles/Ownable.sol";
 
-contract DoctorsRegistry {
+contract DoctorsRegistry is Ownable {
 
     event DoctorRegistered (uint license, address doctorAddress, uint[] categories);
     event DoctorDeleted (uint license, address doctorAddress);
     event DoctorUpdated (address doctorAddress, uint[] categories);
     event ConsultationCreated (uint category, uint date, address doctor, address patient, string prescription);
+    event AdminCreated(address admin);
+    event AdminRemoved(address admin);
 
     TrustCare trustCare;
 
@@ -27,45 +29,99 @@ contract DoctorsRegistry {
 
     mapping (address => Doctor) doctors;
     mapping (bytes32 => Consultation) consultations;
+    mapping (address => bool) admins;
 
-    function registerNewDoctor(address userAddress, uint license, uint[] categories) {
-        Doctor newDoctor;
+    modifier onlyDoctor() {
+        require(isDoctor(msg.sender), "error : this address is not registered as doctor");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(isAdmin(msg.sender), "error : this address is not registered as admin");
+        _;
+    }
+
+    function isDoctor(address userAddress) public view returns (bool) {
+        if (doctors[userAddress].license != 0) {
+            return true;
+        }
+        return false;
+    }
+
+    function isAdmin(address userAddress) public view returns (bool) {
+        return admins[userAddress];
+    }
+
+    function addAdmin(address admin) external onlyOwner {
+        require(!admins[admin], "admin already exists");
+        admins[admin] = true;
+        emit AdminCreated(admin);
+    }
+
+    function removeAdmin(address admin) external onlyOwner {
+        require(admins[admin], "admin doesn't exist");
+        admins[admin] = false;
+        emit AdminRemoved(admin);
+    }
+
+    function registerNewDoctor(address userAddress, uint license, uint[] calldata categories) external onlyAdmin {
+        Doctor memory newDoctor;
         newDoctor.license = license;
         newDoctor.categories = categories;
         doctors[userAddress] = newDoctor;
         emit DoctorRegistered(license, userAddress, categories);
     }
 
-    function deleteDoctor(address userAddress) {
+    function deleteDoctor(address userAddress) external onlyAdmin {
         uint license = doctors[userAddress].license;
         delete doctors[userAddress];
         emit DoctorDeleted(license, userAddress);
     }
 
-    function updateDoctor(address userAddress, uint[] categories) {
+    function updateDoctor(address userAddress, uint[] calldata categories) external onlyAdmin {
         doctors[userAddress].categories = categories;
-        emit DoctorDeleted(userAddress, categories);
+        emit DoctorUpdated(userAddress, categories);
     }
 
-    function doctor(address userAddress) {
-        return doctors[userAddress];
+    function doctorLicense(address userAddress) public view returns (uint) {
+        return doctors[userAddress].license;
     }
-    function addConsultation(uint category, address patient, string prescription) {
+
+    function doctorCategories(address userAddress) public view returns (uint[] memory) {
+        return doctors[userAddress].categories;
+    }
+
+    function addConsultation(uint category, address patient, string calldata prescription) external onlyDoctor {
         uint date = now;
-        address doctor = msg.sender;
-        bytes32 consultationID = keccak256(abi.encode(category, date, doctor, patient, prescription));
-        Consultation newConsultation;
+        address sender = msg.sender;
+        bytes32 consultationID = keccak256(abi.encode(category, date, sender, patient, prescription));
+        Consultation memory newConsultation;
         newConsultation.category = category;
         newConsultation.date = date;
-        newConsultation.doctor = doctor;
+        newConsultation.doctor = sender;
         newConsultation.patient = patient;
         newConsultation.prescription = prescription;
         trustCare.newTransaction(consultationID);
         consultations[consultationID] = newConsultation;
-        emit ConsultationCreated (category, date, doctor, patient, prescription);
+        emit ConsultationCreated (category, date, sender, patient, prescription);
     }
-    function consultation(bytes32 consultationID) {
-        return consultations[consultationID];
+    function consultationCategory(bytes32 consultationID) public view returns (uint) {
+        return consultations[consultationID].category;
     }
 
+    function consultationDate(bytes32 consultationID) public view returns (uint) {
+        return consultations[consultationID].date;
+    }
+
+    function consultationDoctor(bytes32 consultationID) public view returns (address) {
+        return consultations[consultationID].doctor;
+    }
+
+    function consultationPatient(bytes32 consultationID) public view returns (address) {
+        return consultations[consultationID].patient;
+    }
+
+    function consultationPrescription(bytes32 consultationID) public view returns (string memory) {
+        return consultations[consultationID].prescription;
+    }
 }
