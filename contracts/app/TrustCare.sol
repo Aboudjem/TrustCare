81436d835c6d3bbdef3e389d90675c28b41aa971
joachimlebrun <joachim@tokeny.com>
2020-09-23 16:52:14 +0200

    ♻(TrustCare, DoctorsRegistry) modify contracts, contracts are compiling

--- contracts/app/TrustCare.sol
+++ contracts/app/TrustCare.sol
@@ -1,24 +1,63 @@
 pragma solidity 0.6.2;
 
-contract TrustCare {
+import "../roles/Ownable.sol";
+
+contract TrustCare is Ownable{
 
     event TransactionCreated(bytes32 transactionID);
     event StatusUpdated(bytes32 transactionID, uint status);
+    event TrustCareCreated(address doctorsRegistry, address healthInsuranceRegistry, address patientsRegistry);
 
     mapping (bytes32 => uint) transactionStatus;
-    address doctorsRegistry;
+
+    struct Registries{
+        address doctorsRegistry;
+        address healthInsuranceRegistry;
+        address patientsRegistry;
+    }
+
+    Registries registries;
+
+    modifier onlyDoctor() {
+        require(isDoctor(msg.sender), "error : this address is not registered as doctor contract");
+        _;
+    }
+
+    modifier onlyValidator() {
+        require(isValidator(msg.sender), "error : this address is not registered as validator");
+        _;
+    }
 
 
-    function newTransaction (bytes32 transactionID) {
+    constructor (address doctorsRegistry, address healthInsuranceRegistry, address patientsRegistry) public {
+        registries.doctorsRegistry = doctorsRegistry;
+        registries.healthInsuranceRegistry = healthInsuranceRegistry;
+        registries.patientsRegistry = patientsRegistry;
+        emit TrustCareCreated(doctorsRegistry, healthInsuranceRegistry, patientsRegistry);
+    }
+
+    function isDoctor(address userAddress) public view returns (bool) {
+        if (userAddress != registries.doctorsRegistry) {
+            return false;
+        }
+        return true;
+    }
+
+    function isValidator(address userAddress) public view returns (bool) {
+        if (userAddress != registries.healthInsuranceRegistry && userAddress != registries.patientsRegistry) {
+            return false;
+        }
+        return true;
+    }
+
+    function newTransaction (bytes32 transactionID) external onlyDoctor {
         transactionStatus[transactionID] = 1;
         emit TransactionCreated(transactionID);
     }
 
-    function updateTransactionStatus(bytes32 transactionID, uint status) {
+    function updateTransactionStatus(bytes32 transactionID, uint status) external onlyValidator {
         transactionStatus[transactionID] = status;
         emit StatusUpdated(transactionID, status);
     }
-
-
 
 }
