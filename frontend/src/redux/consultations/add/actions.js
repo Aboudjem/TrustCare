c3092a0f703061753096f1086e142754a2cf0dc9
Adam <boudjemaa.adam@gmail.com>
2020-09-24 14:47:51 +0200

    :add:(Registries) Add sdk to Front

--- frontend/src/redux/consultations/add/actions.js
+++ frontend/src/redux/consultations/add/actions.js
@@ -7,6 +7,10 @@ export const NEW_CONSULTATION_SUBMIT_SUCCESS = 'NEW_CONSULTATION_SUBMIT_SUCCESS'
 export const NEW_CONSULTATION_SUBMIT_FAILURE = 'NEW_CONSULTATION_SUBMIT_FAILURE';
 
 
+const ethers = require("ethers");
+const TrustCare = require("TrustCare");
+
+
 export function newInsuranceChangeField(field, value) {
     return {
         type: NEW_CONSULTATION_CHANGE_FIELD,
@@ -69,9 +73,13 @@ export function newInsuranceSubmit(history) {
                     patient_id: patientId,
                     approved_by_patient: false,
                     approved_by_insurance: false,
+                    timestamp: Date.now(),
                     category: fields.category,
                     prescription: fields.prescription
-                }).then(() => {
+                }).then(async() => {
+                    const signer = (new ethers.providers.Web3Provider(window.ethereum)).getSigner();
+                    const trustCare = await TrustCare.at(process.env.REACT_APP_TRUST_CARE_CONTRACT, signer);
+                    await trustCare.addConsultation(fields.category, Date.now() ,patientId, fields.prescription);
                     dispatch(newConsultationSubmitSuccess())
                     history.push('/')
                 }).catch(err => {
